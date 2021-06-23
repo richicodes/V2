f6b3dff2d484db3b1089767052393ae7aa711094
Ri Chi <65701066+richicodes@users.noreply.github.com>
2021-06-23 09:56:34 +0000

    Complete: CSV Upload form for units Revamp of DB to standardise naming convention
    In progress:
    Unit form, queries

--- forms.py
+++ forms.py
@@ -1,15 +1,18 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 
-from wtforms import StringField, SubmitField, FileField, TextAreaField
-from wtforms.validators import InputRequired, UUID, regexp
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import InputRequired, UUID
 
 class uuidForm(FlaskForm):
   uuid  = StringField('SingPass UUID', validators=
     [InputRequired(message="No input to process"), UUID(message="Please input valid UUID")])
   submit = SubmitField('Singpass Login')
 
-''' class unitForm(FlaskForm):
-  file = FileField('Choose File', validators=
-    [regexp(u'^[^/\\]\.csv$', message = "Please choose a .csv file")])
+class csvForm(FlaskForm):
+  file = FileField(validators=[FileRequired("Please choose a file to upload"), FileAllowed(["csv"], "Only .csv files are accepted")])
+  submit = SubmitField('Upload')
+
+class unitForm(FlaskForm):
   entry = TextAreaField(validators=[InputRequired(message="No input to process")])
-  search = SubmitField('Search') '''
+  submit = SubmitField('Search')
--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 from datetime import date
 from dateutil.relativedelta import relativedelta
+from werkzeug.utils import secure_filename
 
 from forms import *
 
-import os, random, math
+import os, random, math, csv
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['secret_key']
@@ -18,37 +19,46 @@ app.config['SQLALCHEMY_BINDS'] = {"intranet":'sqlite:///eMedicIntranet.db'}
 
 db = SQLAlchemy(app)
 
-class Medic(db.Model):
-  __tablename__ = "maskedIC"
-  UUID = db.Column(db.String(), primary_key=True, unique=True, nullable=False)
-  maskedIC = db.Column(db.String(), nullable=False)
-  ampt = db.relationship("AMPT", backref="medic", lazy='dynamic')
-  eligibility = db.relationship("Eligibility", lazy='dynamic')
+class MaskedIC(db.Model):
+  __tablename__ = "masked_ic"
+  uuid = db.Column(db.String(), primary_key=True, unique=True, nullable=False)
+  masked_ic = db.Column(db.String(), nullable=False)
 
 class AMPT(db.Model):
-  __tablename__ = "AMPT"
-  UUID = db.Column(db.String(), db.ForeignKey('maskedIC.UUID'), primary_key=True, unique=True, nullable=False)
-  Date = db.Column(db.Date(), nullable=False)
+  __tablename__ = "ampt"
+  uuid = db.Column(db.String(), db.ForeignKey('masked_ic.uuid'), primary_key=True, unique=True, nullable=False)
+  ampt_date = db.Column(db.Date(), nullable=False)
 
-class Eligibility(db.Model):
-  __tablename__ = "Eligibility"
-  UUID = db.Column(db.String(), db.ForeignKey('maskedIC.UUID'), primary_key=True, unique=True, nullable=False)
-  Date = db.Column(db.Date(), nullable=False)
+class VocDate(db.Model):
+  __tablename__ = "voc_date"
+  uuid = db.Column(db.String(), db.ForeignKey('masked_ic.uuid'), primary_key=True, unique=True, nullable=False)
+  course_date = db.Column(db.Date(), nullable=False)
 
 class AED(db.Model):
   __bind_key__= "intranet"
-  UUID = db.Column(db.String(), db.ForeignKey('maskedIC.UUID'), primary_key=True, unique=True, nullable=False)
-  Date = db.Column(db.Date(), nullable=False)
+  __tablename__ = "aed"
+  uuid = db.Column(db.String(), db.ForeignKey('masked_ic.uuid'), primary_key=True, unique=True, nullable=False)
+  aed_date = db.Column(db.Date(), nullable=False)
+  aed_name = db.Column(db.String(), nullable=False)
+  aed_cert = db.Column(db.String(), unique=True, nullable=False)
 
-class EligibilityCourse(db.Model):
+class VocName(db.Model):
   __bind_key__= "intranet"
-  UUID = db.Column(db.String(), db.ForeignKey('maskedIC.UUID'), primary_key=True, unique=True, nullable=False)
-  Course = db.Column(db.String(), nullable=False)
+  __tablename__ = "voc_name"
+  uuid = db.Column(db.String(), db.ForeignKey('masked_ic.uuid'), primary_key=True, unique=True, nullable=False)
+  course_name = db.Column(db.String(), nullable=False)
 
-class fullName(db.Model):
+class FullName(db.Model):
   __bind_key__= "intranet"
-  UUID = db.Column(db.String(), db.ForeignKey('maskedIC.UUID'), primary_key=True, unique=True, nullable=False)
-  Course = db.Column(db.String(), nullable=False)
+  __tablename__ = "full_name"
+  uuid = db.Column(db.String(), db.ForeignKey('masked_ic.uuid'), primary_key=True, unique=True, nullable=False)
+  full_name = db.Column(db.String(), nullable=False)
+
+class Profile(db.Model):
+  __bind_key__= "intranet"
+  __tablename__ = "profile"
+  uuid = db.Column(db.String(), db.ForeignKey('masked_ic.uuid'), primary_key=True, unique=True, nullable=False)
+  rights = db.Column(db.String(), nullable=False)
 
 # flask routes
 @app.route('/')
@@ -60,43 +70,57 @@ def home():
   form = uuidForm()
   if form.validate_on_submit():
     session['uuid'] = form.uuid.data
-    if Medic.query.filter_by(UUID=session['uuid']).first() == None:
-      return redirect(url_for('invalid'))
     return redirect(url_for('medic'))
   return render_template('home.html', form = form)
 
 @app.route('/medic')
 def medic():
-  MedicQ = Medic.query.filter_by(UUID=session['uuid']).first()
-  ampt = MedicQ.ampt.all()[0].Date
-  eligibility = MedicQ.eligibility.all()[0].Date
-  expDate = max(ampt, eligibility) + relativedelta(years=1)
+  medicQuery = (
+    db.session.query(MaskedIC, AMPT, VocDate)
+    .join(AMPT)
+    .join(VocDate)
+    .filter_by(uuid=session['uuid'])
+    .first())
+  ampt = medicQuery.AMPT.ampt_date
+  voc = medicQuery.VocDate.course_date
+  if ampt == None and voc == None:
+    return redirect(url_for('invalid'))
+  ic = medicQuery.MaskedIC.masked_ic
+  expDate = max(ampt, voc) + relativedelta(years=1)
   duration = (expDate-date.today()).days
   valid = True
   if duration < 0:
     valid = False
     duration *= -1
-  return render_template('medic.html', ic = MedicQ.maskedIC, exp = expDate.strftime("%-d %B %Y"), valid = valid, months = math.floor(duration/30.5), days = duration)
+  return render_template('medic.html', ic = ic, exp = expDate.strftime("%-d %B %Y"), valid = valid, months = math.floor(duration/30.5), days = duration)
 
 @app.route('/inet', methods=['GET', 'POST'])
 def inet():
   form = uuidForm()
   if form.validate_on_submit():
     session['uuid'] = form.uuid.data
-    #if Medic.query.filter_by(UUID=session['uuid']).first() == None:
-    #  return redirect(url_for('invalid'))
     return redirect(url_for('unit'))
   return render_template('inet.html', form = form)
 
-@app.route('/unit')
+@app.route('/unit', methods=['GET', 'POST'])
 def unit():
-  form = unitForm()
-  if form.validate_on_submit():
-    if form.search.data:
-      return redirect(url_for('unit'))
-    elif form.submit.data:
-      return redirect(url_for('unit'))
-  return render_template('unit.html', inet = True, form = form)
+  csvform = csvForm()
+  unitform = unitForm(
+    entry="Masked NRIC, First Word Of Name"
+  )
+  if csvform.validate_on_submit():
+    f = csvform.file.data
+    #try:
+    overwrite = unitForm(
+      entry=csv.reader(
+      f.read().decode("utf-8-sig").split('\n')
+      )
+    )        
+    unitform.populate_obj(overwrite)
+    #except:
+    #  csvform.file.errors.append("The file is formatted incorrectly")
+         
+  return render_template('unit.html', inet = True, csvform = csvform, unitform=unitform)
 
 @app.route('/smti')
 def smti():
